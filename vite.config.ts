628833b5d44959850f50ae27ce315dba1fa023a8
IgorD <avtosklokyiv@gmail.com>
2025-05-13 23:52:12 +0300

    jjj

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,15 @@ export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === "favicon.ico") {
+            return "";
+          }
+          return "[name].[ext]";
+        },
+      },
+    },
   },
 });
